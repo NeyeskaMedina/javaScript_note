9823e471fdcd4a0cc95887a4074dc3c3d6987c6f
Neyeska Medina <neyeska.go@gmail.com>
2023-08-16 19:57:33 -0400

    Add sentence switch

--- javaScript-main/bucles/switch.js
+++ javaScript-main/bucles/switch.js
@@ -1,12 +1,12 @@
 let diaSemana = "Lunes"
 
 switch(){
-    case: diaSemana = "Lunes";
+    case diaSemana = "Lunes":
     console.log("Hoy es lunes");
     break;
-    case: diaSemana = "Martes";
+    case diaSemana = "Martes":
     console.log("Hoy es Martes");
     break;
-    default console.log("No es lunes ni martes");
-    
+    default: console.log("No es lunes ni martes");
+
 }
